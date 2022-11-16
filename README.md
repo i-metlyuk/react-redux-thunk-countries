931f359d5f81903329299d9aace35fd593e3f689
i-metlyuk <igor.m@core-tech.ru>
2022-11-16 11:49:47 +0300

    Finishing feature structure for filters

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # TODO:
 [x] Switch between light and dark theme (rtk feature)
-[] Filters (rtk feature)
+[x] Filters (rtk feature)
 [] Countries (rtk feature)
 [] Get additional info for selected country (rtk feature)
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 
-
 import {Container} from './Container';
-import {useDispatch} from "react-redux";
-import {clearControls} from "../store/controls/controls-actions";
 import ThemeSwitcher from "../features/theme/ThemeSwitcher";
+import {useCleanup} from "../features/controls/useCleanup";
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -29,11 +27,7 @@ const Title = styled(Link).attrs({
 `;
 
 export const Header = () => {
-    const dispatch = useDispatch();
-
-    const cleanUp = () => {
-        dispatch(clearControls());
-    }
+    const cleanUp = useCleanup();
 
     return (
         <HeaderEl>
